970f27c1b377d82f580c0fe6d3468c362e26f06f
Joshua Lively <jlively2011@gmail.com>
2024-02-09 11:07:57 -0600

    add nav, change style

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-const hero = document.querySelector('#hero');
-const left = document.querySelector('#left');
-const top_ = document.querySelector('#top');
-const right = document.querySelector('#right');
-const bottom = document.querySelector('#bottom');
-const hidden = document.querySelector('.hide');
+const hero = document.querySelector('.hero__image');
+const missionLink = document.querySelector('.mission_link');
+const productsLink = document.querySelector('.products_link');
+const hide = document.querySelector('.hide');
 const mission = document.querySelector('.mission__content');
-const whipped = document.querySelector('.whipped__content');
-const balm = document.querySelector('.balm__content');
+const products = document.querySelector('.products__content');
+const lavender = document.querySelector('.lavender');
 
 
-left.addEventListener('click', () => {
+missionLink.addEventListener('click', () => {
     mission.classList.toggle('hide');
-    whipped.classList.add('hide');
-    balm.classList.add('hide');
+    missionLink.classList.toggle('bold');
+    products.classList.add('hide');
     if (!mission.classList.contains('hide')) {
         hero.classList.add('hide');
     } else {hero.classList.remove('hide');}
+    if (productsLink.classList.contains('bold')) {
+        productsLink.classList.remove('bold');
+    }
 });
 
-top_.addEventListener('click', () => {
-    whipped.classList.toggle('hide');
+productsLink.addEventListener('click', () => {
+    products.classList.toggle('hide');
+    productsLink.classList.toggle('bold');
     mission.classList.add('hide');
-    balm.classList.add('hide');
-    if (!whipped.classList.contains('hide')) {
+    if (!products.classList.contains('hide')) {
         hero.classList.add('hide');
     } else {hero.classList.remove('hide');}
-});
-
-bottom.addEventListener('click', () => {
-    balm.classList.toggle('hide');
-    mission.classList.add('hide');
-    whipped.classList.add('hide');
-    if (!balm.classList.contains('hide')) {
-        hero.classList.add('hide');
-    } else {hero.classList.remove('hide');}
+    if (missionLink.classList.contains('bold')) {
+        missionLink.classList.remove('bold');
+    }
 });
